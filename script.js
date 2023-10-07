cf38fbfe7f6774fe55c36e73d8aac77910b2bace
ovin44 <mariaoktavianansedia0@gmail.com>
2023-10-07 08:14:20 -0700

    Poject JS1

---
+++ script.js
@@ -0,0 +1,14 @@
+// if, else, nestad if
+let kondisi1 = true;
+let kondisi2 = false;
+ 
+if (kondisi1) {
+    console.log("kondisi1 benar.");
+    if (kondisi2) {
+    console.log("Kondisi2 salah.");
+    } else {
+    console.log("Kondisi2 salah.");
+}
+} else {
+    console.log("Kondisi1 salah.");
+}
